7168a6d5837bd4c4de5774bfe180c2fe55f9fed6
StephanieLuciaM <marie_lucia@hotmail.fr>
2025-04-29 19:19:32 +0200

    index.js render

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 import "dotenv/config";
 import express from "express";
 import { router } from "./src/routes/index.routes.js";
-import cors from "cors"; // Import cors
+import cors from "cors";
 import cookieParser from "cookie-parser";
-
 import helmet from "helmet";
-import { bodySanitizer } from "./src/middlewares/sanitizeMiddleware.js"; // Import body sanitizer middleware
-
-
-
+import { bodySanitizer } from "./src/middlewares/sanitizeMiddleware.js";
 
 const app = express();
 
@@ -17,13 +13,14 @@ app.use(helmet());
 
 // CORS module: specify who can access the API
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://seniorlove.vercel.app'], // * = allow everyone (not a best practice, but it's fine for local development),
+  origin: ['http://localhost:5173', 'https://seniorlove.vercel.app'],
   credentials: true,
   methods: "GET, PUT, POST, PATCH, DELETE",
   allowedHeaders: "Content-Type, Authorization"
 }));
 
 app.use(cookieParser());
+
 // Body parser configuration (to retrieve form data)
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); // Allow interpreting data provided in a POST, PATCH, or PUT request as JSON
@@ -34,9 +31,13 @@ app.use(bodySanitizer);
 // Use routes
 app.use(router);
 
-
 // Start a server
-const PORT = process.env.PORT || 3000; // Fallback value if process.env.PORT is undefined, default to port 3000
-app.listen(PORT, () => {
-  console.log(`🚀 Server listening at http://localhost:${PORT}`);
+const PORT = process.env.PORT || 3000;
+
+// Log attempt to start server
+console.log(`Attempting to start server on PORT: ${PORT}`);
+
+// Explicitly binding to 0.0.0.0 to ensure the app is accessible from outside the container
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server successfully listening at http://0.0.0.0:${PORT}`);
 });
